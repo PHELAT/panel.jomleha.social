b781ab4a28a1e926b09fa59040d254fc2d94ebe0
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2023-04-05 18:23:26 +0200

    Add firebase login prompt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,20 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { initializeApp } from 'firebase/app';
+import { TwitterAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPgI4pxTLDOM5xnY-OLFRfiHw-7yuur4M",
+  authDomain: "jomlehasocialpanel.firebaseapp.com",
+  projectId: "jomlehasocialpanel",
+  storageBucket: "jomlehasocialpanel.appspot.com",
+  messagingSenderId: "759540614073",
+  appId: "1:759540614073:web:ee058ef6cc801a848f6c68",
+  measurementId: "G-9YSZQEE08T"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new TwitterAuthProvider();
 
 export default function Home() {
   return (
@@ -12,15 +28,24 @@ export default function Home() {
       <div className='flex flex-col h-screen justify-between'>
         <main className='w-full h-full flex items-center justify-center px-8'>
           <div className='flex flex-col gap-4'>
-            <form className='flex flex-col gap-4' id='jomlehform' method='post' action='/api/negaresh'>
-              <textarea className='form-textarea' name='jomleh' placeholder='جمله'></textarea>
-              <input className='form-input' type='text' name='context' placeholder='زمینه'></input>
-              <input className='form-input' type='url' name='link' placeholder='پیوند'></input>
-            </form>
-            <button className='px-4 py-1 text-sm text-zinc-600 font-semibold rounded-none border border-zinc-200 hover:text-white hover:bg-zinc-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:ring-offset-2' type='submit' form='jomlehform'>نگارش</button>
+            <button className='px-4 py-1 text-sm text-zinc-600 font-semibold rounded-none border border-zinc-200 hover:text-white hover:bg-zinc-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:ring-offset-2' onClick={login}>ورود</button>
           </div>
         </main>
       </div>
     </>
   )
+}
+
+function login() {
+  signInWithPopup(auth, provider).then((result: any) => {
+    const credential = TwitterAuthProvider.credentialFromResult(result);
+    // const token = credential!.accessToken;
+    // const secret = credential!.secret;
+    // const user = result.user;
+  }).catch(error => {
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // const email = error.customData.email;
+    // const credential = TwitterAuthProvider.credentialFromError(error);
+  })
 }
---
+++ src/pages/negaresh/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAPgI4pxTLDOM5xnY-OLFRfiHw-7yuur4M",
+    authDomain: "jomlehasocialpanel.firebaseapp.com",
+    projectId: "jomlehasocialpanel",
+    storageBucket: "jomlehasocialpanel.appspot.com",
+    messagingSenderId: "759540614073",
+    appId: "1:759540614073:web:ee058ef6cc801a848f6c68",
+    measurementId: "G-9YSZQEE08T"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default function Negaresh() {
+    return (
+        <>
+            <Head>
+                <title>Jomleha Panel</title>
+                <meta name="description" content="Jomleha Panel" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className='flex flex-col h-screen justify-between'>
+                <main className='w-full h-full flex items-center justify-center px-8'>
+                    <div className='flex flex-col gap-4'>
+                        <form className='flex flex-col gap-4' id='jomlehform' method='post' action='/api/negaresh'>
+                            <textarea className='form-textarea' name='jomleh' placeholder='جمله'></textarea>
+                            <input className='form-input' type='text' name='context' placeholder='زمینه'></input>
+                            <input className='form-input' type='url' name='link' placeholder='پیوند'></input>
+                        </form>
+                        <button className='px-4 py-1 text-sm text-zinc-600 font-semibold rounded-none border border-zinc-200 hover:text-white hover:bg-zinc-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:ring-offset-2' type='submit' form='jomlehform'>نگارش</button>
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
